b2a712b8db00ced7bd89d2f5aceb00d63ce9be16
HiteshShingala <51263039+Hiteshshingala@users.noreply.github.com>
2020-09-26 22:52:25 +0530

    Add files via upload

---
+++ precache-manifest.86a87e5c39833422b18563f4eb049cba.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5432bd39cb7e78982a77a804530d42a4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7525ea789e54ac43b5c2",
+    "url": "/static/css/main.210e264c.chunk.css"
+  },
+  {
+    "revision": "b2b427dae0a777128447",
+    "url": "/static/js/2.2ccb9726.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/static/js/2.2ccb9726.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7525ea789e54ac43b5c2",
+    "url": "/static/js/main.348308ef.chunk.js"
+  },
+  {
+    "revision": "e173dfe45a061c6e2715",
+    "url": "/static/js/runtime-main.20a4ba51.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/static/media/image.d7265326.png"
+  }
+]);
